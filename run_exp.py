06b0f7cdaf389b028a5d9335b69df550c8a924c3
andres <andresgarciar96@gmail.com>
2024-01-11 22:48:38 +0100

    experiment manager now saves generation key stats and the best solution in a text file

--- run_exp.py
+++ run_exp.py
@@ -2,27 +2,27 @@ import os
 import time
 import argparse
 from datetime import datetime
-import pdb
-import math
-import random
 import numpy as np
-from simulate import run_sim
 from evolution import evolve_population
 from utils.logger import log_gen
 
 n_gens = 2
 population = np.random.uniform(2000, 3000, size=(2, 5))
 
-print("-----------------------Beginning Experiment-----------------------")
+# Enter a name in the terminal
+experiment_name = input("Enter name of experiment: ")
+
+# Run experiment
+print(f"*-----------------------Beginning Experiment: {experiment_name}-----------------------*")
 for i in range(n_gens):
     curr_gen = i+1
     print(f"Current Generation: {curr_gen}")
     population, pop_with_fits = evolve_population(i, population)
 
     if curr_gen % 1 == 0:
-        log_gen(curr_gen, pop_with_fits)
+        log_gen(experiment_name, curr_gen, pop_with_fits)
 
-print("-----------------------Experiment Ended-----------------------")
+print("*-----------------------Experiment Ended-----------------------*")
 
 
 
--- utils/logger.py
+++ utils/logger.py
@@ -1,16 +1,37 @@
 import numpy as np
+import os
 
 
-def log_gen(gen, pop_with_fits: np.array):
+def log_gen(exp_name, gen, pop_with_fits: np.array):
 
     print(f"-----------------------Stats at Generation {gen}-----------------------")
     max_idx = np.argmax(pop_with_fits[:, 1])
     min_idx = np.argmin(pop_with_fits[:, 1])
 
-    max_fitness = pop_with_fits[max_idx][1]
+    best_solution = pop_with_fits[max_idx]
+    max_fitness = best_solution[1]
     min_fitness = pop_with_fits[min_idx][1]
     mean_fitness = np.mean(pop_with_fits[:, 1])
 
     print(f"Max Fitness: {max_fitness}")
     print(f"Min Fitness: {min_fitness}")
     print(f"Mean Fitness: {mean_fitness}")
+
+    results_folder = os.path.join(os.path.dirname(os.path.dirname(__file__)), "results")
+
+    # Create or open the text file for logging
+    log_file_path = os.path.join(results_folder, f"{exp_name}_results.txt")
+    with open(log_file_path, "a") as log_file:
+        # If the file doesn't exist, create it and write a header
+        if not os.path.exists(log_file_path):
+            log_file.write("Generation,Max_Fitness,Min_Fitness,Mean_Fitness\n")
+
+        # Write the information in the same line
+        log_file.write(f"{gen},{max_fitness},{min_fitness},{mean_fitness}, {best_solution}\n")
+
+
+# exp_name = "Experiment1"
+# gen = 1
+# pop_with_fits = np.random.rand(10, 2)  # Replace this with your actual data
+#
+# log_gen(exp_name, gen, pop_with_fits)
